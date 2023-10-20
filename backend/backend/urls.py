b37cc4f93b6ccb8f74106d4fb017001cb50e173d
sheparov ilya <sheparov.71@mail.ru>
2023-10-20 14:46:27 +0300

    add registration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
+    path('users/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
