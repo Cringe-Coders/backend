13907768ff2b63a542e2bb8a026ef5595c3850ae
sheparov ilya <sheparov.71@mail.ru>
2023-10-20 14:41:48 +0300

    add jwt authorization

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 
+from .managers import CustomUserManager
+
 
 def user_avatar_directory_path(instance: "User", filename: str) -> str:
     return f"users/user_{instance.id}/avatar/{filename}"
@@ -11,6 +13,7 @@ class User(AbstractUser):
     avatar = models.ImageField(blank=True, upload_to="users_photos")
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
+    objects = CustomUserManager()
 
     def get_absolute_url(self):
         return reverse("users:profile", kwargs={"pk": self.pk})
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from users.models import User
+
+
+class SignUpView(APIView):
+    def post(self, request: Request):
+        user_data = json.loads(request.body)
+        username = user_data.get("username")
+        password = user_data.get("password")
+        user = User.objects.get_or_none(username=username)
+        if user is None:
+            user = User.objects.create_user(username=username, password=password)
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
